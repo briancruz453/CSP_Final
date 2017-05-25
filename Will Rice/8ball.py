07bf458511e9a5b3b8b1b05d70101e2703449712
William.Rice <wrice03@gmail.com>
2017-05-25 10:19:15 -0500

    commented program to determine how it works

--- Will Rice/8ball.py
+++ Will Rice/8ball.py
@@ -1,6 +1,6 @@
-import random
+import random       #random number generator
 
-ANSWERS = [
+ANSWERS = [                                #below are the various answers that the program can output, randomly
     "It is certain",
     "It is decidedly so",
     "Without a doubt",
@@ -22,8 +22,14 @@ ANSWERS = [
     "Outlook not so good",
     "Very doubtful" ]
 
-def main():
+def main():      #defines the main and only task
     print(ANSWERS[random.randint(0, len(ANSWERS) - 1)])
+    #prints out the actual text answer in console. the program assigns a random integer to each answer above and it uses the import
+    #random to choose a random response
+    #len(ANSWERS) insures that no matter how many answers are added above the program still works
 
 if __name__ == "__main__":
     main()
+#without this the file doesnt work. if this program is ran alone it gets assigned the name "__main__". if this is not true then
+#the program will not run. if it is true it will run Main(), which is the defination above. this will then execute that code to 
+#run the 8 ball ;)
